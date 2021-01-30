5ee29f14cf4238b5dd7244874f84c22fac447a3a
jvmfdev <72178115+jvmfdev@users.noreply.github.com>
2021-01-30 06:44:17 -0600

    Intro to Express Part2

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
+//morgan manage the logs request
+app.use(morgan('dev'));
+
+//serve files from public folder
+app.use(express.static(__dirname +'/public'));
+
 //use method
+//__dirname refers to the absolute path of the current directory of the file
+// that it is in
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
